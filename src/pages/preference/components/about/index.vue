e6c6caebc7b609024fa9607bee051a8a53b119e1
0xhacker <2022302121222@whu.edu.cn>
2026-04-06 22:06:39 +0800

    chore: update application version to 0.0.1, change GitHub link, and clean up unused code in about component

--- src/pages/preference/components/about/index.vue
+++ src/pages/preference/components/about/index.vue
@@ -1,13 +1,7 @@
 <script setup lang="ts">
-import { getTauriVersion } from '@tauri-apps/api/app'
 import { emit } from '@tauri-apps/api/event'
-import { appLogDir } from '@tauri-apps/api/path'
-import { writeText } from '@tauri-apps/plugin-clipboard-manager'
-import { openPath, openUrl } from '@tauri-apps/plugin-opener'
-import { arch, platform, version } from '@tauri-apps/plugin-os'
-import { Button, message } from 'ant-design-vue'
-import { onMounted, ref } from 'vue'
-import { useI18n } from 'vue-i18n'
+import { openUrl } from '@tauri-apps/plugin-opener'
+import { Button } from 'ant-design-vue'
 
 import ProList from '@/components/pro-list/index.vue'
 import ProListItem from '@/components/pro-list-item/index.vue'
@@ -15,30 +9,9 @@ import { GITHUB_LINK, LISTEN_KEY } from '@/constants'
 import { useAppStore } from '@/stores/app'
 
 const appStore = useAppStore()
-const logDir = ref('')
-const { t } = useI18n()
-
-onMounted(async () => {
-  logDir.value = await appLogDir()
-})
 
 function handleUpdate() {
   emit(LISTEN_KEY.UPDATE_APP)
-}
-
-async function copyInfo() {
-  const info = {
-    appName: appStore.name,
-    appVersion: appStore.version,
-    tauriVersion: await getTauriVersion(),
-    platform: platform(),
-    platformArch: arch(),
-    platformVersion: version(),
-  }
-
-  await writeText(JSON.stringify(info, null, 2))
-
-  message.success(t('pages.preference.about.hints.copySuccess'))
 }
 
 function feedbackIssue() {
@@ -69,15 +42,6 @@ function feedbackIssue() {
       </template>
     </ProListItem>
 
-    <ProListItem
-      :description="$t('pages.preference.about.hints.appInfo')"
-      :title="$t('pages.preference.about.labels.appInfo')"
-    >
-      <Button @click="copyInfo">
-        {{ $t('pages.preference.about.buttons.copy') }}
-      </Button>
-    </ProListItem>
-
     <ProListItem :title="$t('pages.preference.about.labels.openSource')">
       <Button
         danger
@@ -91,15 +55,6 @@ function feedbackIssue() {
           {{ GITHUB_LINK }}
         </a>
       </template>
-    </ProListItem>
-
-    <ProListItem
-      :description="logDir"
-      :title="$t('pages.preference.about.labels.appLog')"
-    >
-      <Button @click="openPath(logDir)">
-        {{ $t('pages.preference.about.buttons.viewLog') }}
-      </Button>
     </ProListItem>
   </ProList>
 </template>
